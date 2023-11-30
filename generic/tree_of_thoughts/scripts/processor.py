e828d32fc28b6d6665d08ae07974b0d8e2e6ccbc
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-11-30 16:33:35 +0100

    Update processor.py

--- generic/tree_of_thoughts/scripts/processor.py
+++ generic/tree_of_thoughts/scripts/processor.py
@@ -4,11 +4,6 @@ from lollms.types import MSG_TYPE
 from lollms.personality import APScript, AIPersonality
 import subprocess
 from pathlib import Path
-import os
-import sys
-sd_folder = Path(__file__).resolve().parent.parent / "sd"
-sys.path.append(str(sd_folder))
-import urllib.parse
 import urllib.request
 import json
 import time
@@ -18,9 +13,6 @@ import sys
 import yaml
 import re
 import random
-
-import networkx as nx
-import matplotlib.pyplot as plt
 
 
 def find_matching_number(numbers, text):
@@ -45,9 +37,9 @@ class Processor(APScript):
                  callback = None,
                 ) -> None:
         personality_config_template = ConfigTemplate([
-                {"name":"max_thought_size","type":"int","value":50, "min":10, "max":personality.model.config["ctx_size"]},
-                {"name":"max_judgement_size","type":"int","value":50, "min":10, "max":personality.model.config["ctx_size"]},
-                {"name":"max_summary_size","type":"int","value":50, "min":10, "max":personality.model.config["ctx_size"]},
+                {"name":"max_thought_size","type":"int","value":512, "min":10, "max":personality.model.config["ctx_size"]},
+                {"name":"max_judgement_size","type":"int","value":512, "min":10, "max":personality.model.config["ctx_size"]},
+                {"name":"max_summary_size","type":"int","value":512, "min":10, "max":personality.model.config["ctx_size"]},
                 {"name":"nb_samples_per_idea","type":"int","value":3, "min":2, "max":100},
                 {"name":"nb_ideas","type":"int","value":3, "min":2, "max":100}
             ])
@@ -92,44 +84,6 @@ class Processor(APScript):
                                 top_p=self.personality.model_top_p if top_p is None else top_p,
                                 repeat_penalty=self.personality.model_repeat_penalty if repeat_penalty is None else repeat_penalty,
                                 ).strip()     
-        
-    def add_graph_level(self, G, ideas, prev_selected_id):
-        # Get the nodes at the current level
-        level_nodes = list(G.nodes)[-len(ideas):]
-
-        for i, idea in enumerate(ideas):
-            # Generate a new idea
-            new_idea = f'Idea_{len(G.nodes) + 1}'
-
-            # Add the new idea to the graph
-            G.add_node(new_idea)
-
-            if prev_selected_id>=0:
-                # Connect the selected node at the previous level to the new idea
-                G.add_edge(level_nodes[prev_selected_id], new_idea)
-
-    def visualize_thought_graph(self, G):
-        # Set the positions of the graph nodes
-        pos = nx.spring_layout(G)
-
-        # Draw the graph
-        nx.draw(G, pos, with_labels=True, node_color='lightblue')
-
-        # Highlight the selected node
-        nx.draw_networkx_nodes(G, pos, node_color='red')
-
-        # Draw edges with different styles for selected and unselected nodes
-        edge_labels = {}
-        for edge in G.edges:
-            nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color='red', arrowstyle='->', width=2)
-            edge_labels[edge] = edge[1]
-
-        # Add edge labels
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-
-        # Display the graph
-        plt.axis('off')
-        plt.show()
 
     def run_workflow(self, prompt, previous_discussion_text="", callback=None):
         """
@@ -148,71 +102,62 @@ class Processor(APScript):
         """
         self.callback = callback
         self.bot_says = ""
-        # Create an empty graph
-        thought_graph = nx.DiGraph()
-
         # 1 first ask the model to formulate a query
         final_ideas = []
         summary_prompt = ""
         prev_number = -1
         layers = []
         selections = []
+        output = ""
         for j in range(self.personality_config.nb_ideas):
-            print(f"============= Starting level {j} of the tree =====================")
-            self.step_start(f"Processing Level {j} of the tree")
+            print(f"============= Starting level {j+1} of the tree =====================")
+            output += f"\n-- level {j+1} ---\n"
+            self.full(output)
+            self.step_start(f"Processing Level {j+1} of the tree")
             local_ideas=[]
             judgement_prompt = f"!@>prompt: {prompt}\n"
             for i in range(self.personality_config.nb_samples_per_idea):
+                self.step_start(f"Generating idea number {j+1}:{i+1}/{self.personality_config.nb_samples_per_idea}")
                 print(f"\nIdea {i+1}")
                 if len(final_ideas)>0:
                     final_ideas_text = "\n".join([f'Idea {n}:{i}' for n,i in enumerate(final_ideas)])
-                    idea_prompt = f"""!@>Instructions: Write the next idea. Please give a single idea. 
-!@>prompt: {prompt}
+                    idea_prompt = f"""!@>instructions: given the following discussion and previous ideas, try to give another idea to solve the proposed problem or to enritch the discussion. 
+!@>discussion:
+{previous_discussion_text}
 !@>previous ideas: {final_ideas_text}
 !@>idea:"""
                 else:
-                    idea_prompt = f"""!@>Instruction: 
-Write the next idea. Please give a single idea. 
-!@>prompt:{prompt}
+                    idea_prompt = f"""!@>instructions: given the following discussion, try to give an original idea to solve the proposed problem or to enritch the discussion. 
+!@>discussion:
+{previous_discussion_text}
 !@>idea:"""
                 idea = self.generate(idea_prompt,self.personality_config.max_thought_size)
-                self.full(f"Idea: {idea}: {idea}", callback)
-
+                output += f"\n## Idea {i+1}:\n {idea}\n"
+                self.full(output)
                 local_ideas.append(idea.strip())
                 judgement_prompt += f"\n!@>Idea {i}:{idea}\n"
-            prompt_ids = ",".join([str(i) for i in range(self.personality_config["nb_samples_per_idea"])])
-            judgement_prompt += f"""
-!@>Instructions: Which idea seems the most approcpriate. Answer the question by giving the best idea number without explanations. What is the best idea number {prompt_ids}?
-!@>judgement: The best idea is idea number"""
-            # print(judgement_prompt)
-            self.bot_says = ""
-            best_local_idea = self.generate(judgement_prompt,self.personality_config.max_judgement_size, temperature = 0.1, top_k=1).strip()
-            number, index = find_matching_number([i for i in range(self.personality_config["nb_samples_per_idea"])], best_local_idea)
-            if index is not None and number>=0 and number<len(local_ideas):
-                print(f"Chosen thought n:{number}")
-                final_ideas.append(local_ideas[number]) 
-                
-                if callback is not None:
-                    callback(f"Best local idea:\n{best_local_idea}", MSG_TYPE.MSG_TYPE_STEP)
+                self.step_end(f"Generating idea number {j+1}:{i+1}/{self.personality_config.nb_samples_per_idea}")
+
+            idea_id = self.multichoice_question(f"What is the most adequate idea to the context?\n",[f"{i} - {local_idea}" for local_idea in local_ideas],previous_discussion_text)
+            if idea_id>=0 and idea_id<len(local_ideas):
+                print(f"Chosen thought n:{idea_id}")
+                final_ideas.append(local_ideas[idea_id])
+                self.step(f"Best local idea:\n{local_ideas[idea_id]}")
             else:
                 print("Warning, the model made a wrong answer, taking random idea as the best")
-                number = random.randint(0,self.personality_config["nb_samples_per_idea"])
-                print(f"Chosen thought n:{number}")
-                if number>=0 and number<len(local_ideas):
-                    final_ideas.append(local_ideas[number]) 
+                idea_id = random.randint(0,self.personality_config["nb_samples_per_idea"])
+                print(f"Chosen thought n:{idea_id+1}")
+                if idea_id>=0 and idea_id<len(local_ideas):
+                    final_ideas.append(local_ideas[idea_id]) 
                 else:
                     final_ideas.append(local_ideas[0]) 
-                if callback is not None:
-                    callback(f"!@> Best local idea:\n{best_local_idea}", MSG_TYPE.MSG_TYPE_STEP)
-
+            output += f"Best idea : {idea_id}"
+            self.full(output)
             layers.append(local_ideas)
-            selections.append(number)
-            self.add_graph_level(thought_graph, local_ideas, prev_number)
-            prev_number = number
+            selections.append(idea_id)
             
-            self.step_end(f"Processing Level {j} of the tree")
+            self.step_end(f"Processing Level {j+1} of the tree")
 
-        self.visualize_thought_graph(thought_graph)
         self.step_start(f"Building final summary")
         summary_prompt += "!@>Instructions: Combine these ideas in a comprihensive essai. Give a detailed explanation.\n"
         for idea in final_ideas:
@@ -223,8 +168,8 @@ Write the next idea. Please give a single idea.
 
         ASCIIColors.success("Summary built successfully")
         self.step_end(f"Building final summary")
-        
-        self.full(final_summary, callback)
+        output += f"## Final summary:\n{summary_prompt}"
+        self.full(output)
         
         
         tree_full_output = {
