67a62dc6bf1d4417da1f2fb2e1c6c9a09f7404f6
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-12-02 00:52:30 +0100

    borg queen ready

--- control/queen_of_the_borg/scripts/processor.py
+++ control/queen_of_the_borg/scripts/processor.py
@@ -1,6 +1,16 @@
 from lollms.helpers import ASCIIColors
 from lollms.config import TypedConfig, BaseConfig, ConfigTemplate
 from lollms.personality import APScript, AIPersonality
+from lollms.utilities import PackageManager
+from pathlib import Path
+from typing import List
+if PackageManager.check_package_installed("pygame"):
+    import pygame
+else:
+    PackageManager.install_package("pygame")
+    import pygame
+
+pygame.mixer.init()
 import subprocess
 
 # Helper functions
@@ -24,6 +34,8 @@ class Processor(APScript):
         # options can be added using : "options":["option1","option2"...]        
         personality_config_template = ConfigTemplate(
             [
+                
+                {"name":"nb_attempts","type":"int","value":5, "help":"Maximum number of attempts to summon a drone"},
             ]
             )
         personality_config_vals = BaseConfig.from_template(personality_config_template)
@@ -55,6 +67,22 @@ class Processor(APScript):
         # subprocess.run(["pip", "install", "--upgrade", "-r", str(requirements_file)])      
         ASCIIColors.success("Installed successfully")        
 
+    def mounted(self):
+        """
+        triggered when mounted
+        """
+        pass
+
+    def play_mp3(self, file_path):
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+
+    def selected(self):
+        """
+        triggered when mounted
+        """
+        self.play_mp3(Path(__file__).parent.parent/"assets"/"borg_threat.mp3")
+
     def help(self, prompt="", full_context=""):
         self.full(self.personality.help)
     
@@ -77,9 +105,41 @@ class Processor(APScript):
         Returns:
             None
         """
-        ASCIIColors.info("Generating")
         self.callback = callback
-        out = self.fast_gen(previous_discussion_text,show_progress=True)
-        self.full(out)
-        return ""
+        ASCIIColors.info("Generating")
+        collective:List[AIPersonality] = self.personality.app.mounted_personalities
+
+
+        q_prompt = "!@>You are the queen of borg.\nYou have access to the following assimilated drones:\n"
+        collective_infos = ""
+        for i,drone in enumerate(collective):
+            collective_infos +=  f"drone id: {i}\n"
+            collective_infos +=  f"drone name: {drone.name}\n"
+            collective_infos +=  f"drone description: {drone.personality_description[:126]}...\n"
+        q_prompt += collective_infos
+        answer = ""
+        q_prompt += f"You are a great leader and you know which drone is most suitable to answer the user request.\n"
+        q_prompt += f"!@>user:{prompt}\n"
+        q_prompt += "!@>Queen of borg: To answer the user I summon the drone with id "
+        attempts = 0
+        self.step_start("Summoning collective")
+        while attempts<self.personality_config.nb_attempts:
+            try:
+                selection = int(self.fast_gen(q_prompt, 3,show_progress=True).split()[0].split(",")[0])
+                self.step_end("Summoning collective")
+                self.step(f"Selected drone {collective[selection]}")
+                collective[selection].callback=callback
+                collective[selection].new_message("")
+
+                if collective[selection].processor:
+                    collective[selection].processor.text_files = self.text_files
+                    collective[selection].processor.image_files = self.image_files
+                    collective[selection].processor.run_workflow(prompt, previous_discussion_text, callback)
+                else:
+                    collective[selection].generate(previous_discussion_text,self.personality.config.ctx_size-len(self.personality.model.tokenize(previous_discussion_text)),callback=callback)
+                break
+            except:
+                self.step_end("Summoning collective", False)
+                attempts += 1
+        return answer
 
