3b3a5d7f1bcdf0c987be9335dfb19c624907654a
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-12-26 02:00:33 +0100

    added electricity guru

---
+++ electronics/electricity_guru/scripts/processor.py
@@ -0,0 +1,86 @@
+from lollms.helpers import ASCIIColors
+from lollms.config import TypedConfig, BaseConfig, ConfigTemplate
+from lollms.personality import APScript, AIPersonality
+import subprocess
+
+# Helper functions
+class Processor(APScript):
+    """
+    A class that processes model inputs and outputs.
+
+    Inherits from APScript.
+    """
+    def __init__(
+                 self, 
+                 personality: AIPersonality,
+                 callback = None,
+                ) -> None:
+        
+        self.callback = None
+        # Example entries
+        #       {"name":"make_scripted","type":"bool","value":False, "help":"Makes a scriptred AI that can perform operations using python script"},
+        #       {"name":"make_scripted","type":"bool","value":False, "help":"Makes a scriptred AI that can perform operations using python script"},
+        # Supported types:
+        # str, int, float, bool, list
+        # options can be added using : "options":["option1","option2"...]        
+        personality_config_template = ConfigTemplate(
+            [
+            ]
+            )
+        personality_config_vals = BaseConfig.from_template(personality_config_template)
+
+        personality_config = TypedConfig(
+            personality_config_template,
+            personality_config_vals
+        )
+        super().__init__(
+                            personality,
+                            personality_config,
+                            [
+                                {
+                                    "name": "idle",
+                                    "commands": { # list of commands (don't forget to add these to your config.yaml file)
+                                        "help":self.help,
+                                    },
+                                    "default": None
+                                },                           
+                            ],
+                            callback=callback
+                        )
+        
+    def install(self):
+        super().install()
+        
+        # requirements_file = self.personality.personality_package_path / "requirements.txt"
+        # Install dependencies using pip from requirements.txt
+        # subprocess.run(["pip", "install", "--upgrade", "-r", str(requirements_file)])      
+        ASCIIColors.success("Installed successfully")        
+
+    def help(self, prompt="", full_context=""):
+        self.full(self.personality.help)
+    
+    def add_file(self, path, callback=None):
+        """
+        Here we implement the file reception handling
+        """
+        super().add_file(path, callback)
+
+    def run_workflow(self, prompt, previous_discussion_text="", callback=None):
+        """
+        Runs the workflow for processing the model input and output.
+
+        This method should be called to execute the processing workflow.
+
+        Args:
+            prompt (str): The input prompt for the model.
+            previous_discussion_text (str, optional): The text of the previous discussion. Default is an empty string.
+            callback a callback function that gets called each time a new token is received
+        Returns:
+            None
+        """
+        self.personality.info("Generating")
+        self.callback = callback
+        out = self.fast_gen(previous_discussion_text)
+        self.full(out)
+        return out
+
