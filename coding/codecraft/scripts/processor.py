211dc1c9c449e528eabe04a4592a025f7737a78c
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-01-10 16:50:25 +0100

    script template

--- coding/codecraft/scripts/processor.py
+++ coding/codecraft/scripts/processor.py
@@ -15,7 +15,6 @@ class Processor(APScript):
                  personality: AIPersonality,
                  callback = None,
                 ) -> None:
-        
         self.callback = None
         # Example entries
         #       {"name":"make_scripted","type":"bool","value":False, "help":"Makes a scriptred AI that can perform operations using python script"},
@@ -25,6 +24,7 @@ class Processor(APScript):
         # options can be added using : "options":["option1","option2"...]        
         personality_config_template = ConfigTemplate(
             [
+                {"name":"template_file","type":"str","value":"", "help":"A template file is a file that contains example of the structure of the code to generate. Just link to a file. The file should contain a structuire and placeholders put as comments. # Placeholder: Here you do this thing. You can place multiple placeholders in your code. Make sure the code is not very long as you may envounter context size problems."},
             ]
             )
         personality_config_vals = BaseConfig.from_template(personality_config_template)
@@ -47,6 +47,16 @@ class Processor(APScript):
                             ],
                             callback=callback
                         )
+        self.settings_updated()
+    def settings_updated(self):
+        """
+        
+        """
+        if self.personality_config.template_file!="":
+            with open(self.personality_config.template_file,"r") as f:
+                self.template = f.read()
+        else:
+            self.template=None
         
     def install(self):
         super().install()
@@ -89,9 +99,38 @@ class Processor(APScript):
         Returns:
             None
         """
+        if context_details is None:
+            self.full("<b>The context details is none. This is probably due to the fact that you are using an old version of lollms. Please upgrade lollms to use this persona.</b>")
+            return ""
         self.personality.info("Generating")
         self.callback = callback
-        out = self.fast_gen(previous_discussion_text)
+        if self.template is not None:
+            crafted_prompt=self.build_prompt([
+                                    context_details["conditionning"],
+                                    context_details["user_description"],
+                                    "!@>template: \n"+self.template,
+                                    context_details["documentation"],
+                                    context_details["knowledge"],
+                                    context_details["discussion_messages"],
+                                    context_details["positive_boost"],
+                                    context_details["negative_boost"],
+                                    context_details["force_language"],
+                                    context_details["ai_prefix"],
+            ],5)
+        else:
+            crafted_prompt=self.build_prompt([
+                                    context_details["conditionning"],
+                                    context_details["user_description"],
+                                    context_details["documentation"],
+                                    context_details["knowledge"],
+                                    context_details["discussion_messages"],
+                                    context_details["positive_boost"],
+                                    context_details["negative_boost"],
+                                    context_details["force_language"],
+                                    context_details["ai_prefix"],
+            ],5)
+
+        out = self.fast_gen(crafted_prompt)
         self.full(out)
         return out
 
