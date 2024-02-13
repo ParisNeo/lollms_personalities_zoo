dc80c0fc674593ed879331ebbee62c27331caa4e
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-13 22:02:58 +0100

    enhanced persona

--- data/dataops_pythonista/scripts/processor.py
+++ data/dataops_pythonista/scripts/processor.py
@@ -9,6 +9,8 @@ from lollms.config import TypedConfig, BaseConfig, ConfigTemplate
 from lollms.personality import APScript, AIPersonality, MSG_TYPE
 import subprocess
 from typing import Callable
+import random
+from ascii_colors import get_trace_exception
 
 # Helper functions
 class Processor(APScript):
@@ -93,6 +95,24 @@ class Processor(APScript):
                 outputs.append(str(df.columns.tolist()))
         
         return outputs
+    def get_types(self,files)->str:
+        """
+        This function takes a list of files, output folder path, and output path URL as input.
+        It performs the user request based on the content of the files and returns the output in the specified format.
+        If the user asks for plotting, it uses matplotlib to create a chart and saves it as a png in the output folder.
+        If the user asks for computing something, it builds the code to perform the computation and returns the result as markdown code.
+        """
+        import pandas as pd
+        outputs = []
+        for file in files:
+            if file.suffix == ".csv" or file.suffix == ".txt":
+                df = pd.read_csv(file)
+                outputs.append(str(df.dtypes))
+            elif file.suffix == ".xlsx":
+                df = pd.read_excel(file)
+                outputs.append(str(df.dtypes))
+        
+        return outputs
     def run_workflow(self, prompt:str, previous_discussion_text:str="", callback: Callable[[str, MSG_TYPE, dict, list], bool]=None, context_details:dict=None):
         """
         This function generates code based on the given parameters.
@@ -124,10 +144,10 @@ class Processor(APScript):
         if len(self.personality.text_files)>0:
             done=False
             fails = 0
-            cols = self.get_cols(self.personality.text_files)
+            types = self.get_types(self.personality.text_files)
             files_infos = ""
-            for file, cols in zip(self.personality.text_files, cols):
-                files_infos += f"{file}: {cols}"
+            for file, types_ in zip(self.personality.text_files, types):
+                files_infos += f"file name:{file}\ncolumns with types:\n{types_}"
             while not done and fails < self.personality_config.max_fails:
                 self.step_start(f"Building code, attempt {fails}")
                 try:
@@ -136,36 +156,32 @@ class Processor(APScript):
                         context_details["discussion_messages"],
                         "\n".join([
                         f"Build a python function called reply_to_user to perform the user request given the list of files that he provides:",
-                        "The function returns a string that contains the output.",
                         "The output should be crafted out of the data contained in one or multiple files depending on the user demand.",
-                        "The function should use the inputs or the content of the files to answer the user.",
-                        "for graphs generation, save the output as a png to output_folder/output_image_i.png where i is an index such that the files does not yet exists.",
-                        "return an img tag of the built graphic in html that points to output_root_url/output_image_i.png.",
-                        "if you need to put the output inside a file that is not an image, just return a hyper link to it.",
-                        "if some value needs to be computed, build the code that does this and return the result as markdown code.",
-                        "use the file type to infer which libraries should be used",
+                        "return a html text depicting the output.",
+                        "It can be text or formatted html like table or img tags pointing to the saved plots or a tags pointing to generated file.",
                         "Files list with their columns:",
                         f"{files_infos}",
-                        "make sure you use the right method to load the files depending on their extension",
-                        "It is mandatory to import the following:",
-                        "from pathlib import Path",
-                        "from typing import List",
-                        "import pandas as pd",
                         f"!@>previous code attempt:\n```python\n{code}\n```\n" if code!="" else "",
-                        f"!@>detected bug: {previous_error}" if previous_error != "" else "",
+                        f"!@>detected bug:\n{previous_error}" if previous_error != "" else "",
                         ]),
-                        "def reply_to_user(files:List[Path], output_folder:Path, output_root_url:str)->str:"
+                        "def reply_to_user(files, output_folder)->str:",
+                        "\n".join([
+                            "import pandas as pd",
+                        ])                        
                         )
-                    output_folder = self.personality.lollms_paths.personal_outputs_path/self.personality.personality_folder_name
+                    rnd_val=f"outgen_{random.randint(0,2000000000)}"
+                    output_folder = self.personality.lollms_paths.personal_outputs_path/self.personality.personality_folder_name/rnd_val
                     output_folder.mkdir(exist_ok=True, parents=True)
-                    out = module.reply_to_user(self.personality.text_files, output_folder, f"/outputs/{self.personality.personality_folder_name}")
+                    out = module.reply_to_user([str(f) for f in self.personality.text_files], str(output_folder))
+                    out= out.replace(str(output_folder),f"/outputs/{self.personality.personality_folder_name}/{rnd_val}")
+                    
                     if self.personality_config.show_code:
                         out = f"```python\n{code}\n```\n"+out
                     self.full(out)
                     done = True
                     self.step_end(f"Building code, attempt {fails}")
                 except Exception as ex:
-                    previous_error=str(ex)
+                    previous_error=str(get_trace_exception(ex))
                     self.step_end(f"Building code, attempt {fails}",False)
                     fails += 1
                     ASCIIColors.error(str(ex))
