b34c7dc9156166b055d76e1b7069c5656b9d80ba
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-05-11 11:29:58 +0200

    enahnced internet

--- internet/rss_feed_fuser/scripts/processor.py
+++ internet/rss_feed_fuser/scripts/processor.py
@@ -409,7 +409,8 @@ Article classified as : {cats[answer]}
             self.step_end("Understanding request")
             self.step("Decided to make an internet search")
             self.personality.step_start("Crafting internet search query")
-            query = self.personality.fast_gen(f"!@>discussion:\n{previous_discussion_text}\n!@>system: Read the discussion and craft a web search query suited to recover needed information to reply to last {self.personality.config.user_name} message.\nDo not answer the prompt. Do not add explanations.\n!@>current date: {datetime.now()}!@>websearch query: ", max_generation_size=256, show_progress=True, callback=self.personality.sink).split("\n")[0]
+            query = self.personality.fast_gen(f"!@>discussion:\n{previous_discussion_text}\n!@>system: Read the discussion and craft a web search query suited to recover needed information to reply to last {self.personality.config.user_name} message.\nDo not answer the prompt. Do not add explanations.\n!@>current date: {datetime.now()}\n!@>websearch query: ", max_generation_size=256, show_progress=True, callback=self.personality.sink).split("\n")[0]
+            self.personality.step("Query: "+query)
             self.personality.step_end("Crafting internet search query")
 
             self.personality.step_start("Scraping (this may take time, so be patient) ....")
